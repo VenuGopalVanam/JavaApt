be2fd94e905c454f8e090912575196fbc0e4e433
VenuGopalVanam <venugopal.vanam5@gmail.com>
2025-11-19 15:07:30 +0530

    commit day 19

---
+++ src/org/tasks/day19/KeyWords.java
@@ -0,0 +1,10 @@
+package org.tasks.day19;
+
+public class KeyWords {
+
+	public static void main(String[] args) {
+		int a = 10;
+		float b = 2.2f;
+		System.out.println("Integer:"+a+" "+"Float:"+b);
+	}
+}
